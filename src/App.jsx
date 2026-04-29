0b3089d2803d9c6096377d24faa57dbafe8896fe
Adnan Anjum <anjumcode12@gmail.com>
2026-04-29 10:26:56 +0500

    add files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-gray-900 text-white border-amber-500 border-2'>
+      hello World
+    </div>
+  )
+}
+
+export default App
